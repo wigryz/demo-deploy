628a9ea305df3ad40a29052bfcb205bbc5f1d66f
Tomasz Ligęza <tomasz.ligeza3@gmail.com>
2023-04-15 14:34:25 +0200

    add script for deploy

--- CodeDeploy/scripts/fix_privileges.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-chmod +x /home/ubuntu/server/* jar
-chmod +x /home/ubuntu/server/server_start.sh
-chmod +x /home/ubuntu/server/server_stop.sh
---
+++ make_deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+cp target/*.war CodeDeploy
+cd CodeDeploy && zip ../demo-deploy.zip *.war appspec.yml scripts/
