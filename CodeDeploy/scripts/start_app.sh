b477a0fe96fb08bd1d1515709c641b2d87621ee3
Tomasz Ligęza <tomasz.ligeza3@gmail.com>
2023-04-17 09:51:36 +0200

    up

--- CodeDeploy/scripts/start_app.sh
+++ CodeDeploy/scripts/start_app.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/server || return 1
-sudo java -jar -Dserver.port=80 *.jar > /dev/null 2> /dev/null < /dev/null &
+touch /home/ubuntu/server/log.txt
+sudo java -jar -Dserver.port=80 *.war > /home/ubuntu/server/log.txt 2> /home/ubuntu/server/log.txt < /home/ubuntu/server/log.txt &
