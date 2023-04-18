e04dbb93b3d0f698ddb250b46b6f8baec5980f74
Tomasz Ligęza <tomasz.ligeza3@gmail.com>
2023-04-18 16:22:13 +0200

    changes

--- src/main/java/com/example/demodeploy/HelloController.java
+++ src/main/java/com/example/demodeploy/HelloController.java
@@ -5,12 +5,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping()
 public class HelloController {
 
     @GetMapping(value = "hello")
-    public String hello() {
-        return "hello";
+    public String hello()  {
+        String lhAddress = "";
+        String lhHost = "";
+        try {
+            lhHost = InetAddress.getLocalHost().getHostAddress();
+            lhAddress = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+        }
+        String lbAddress = InetAddress.getLoopbackAddress().getHostAddress();
+        String lbHost = InetAddress.getLoopbackAddress().getHostName();
+        return "LH: A: " + lhAddress + " H: " + lhHost + " ___ LB: A: " + lbAddress + " H: " + lbHost + ".";
     }
 }
