f252b21e8ea5b988cbd68ccd6a20e3ef0ef8fc8f
Tomasz Ligęza <tomasz.ligeza3@gmail.com>
2023-04-25 11:26:51 +0200

    download .env file from s3

--- src/main/java/com/example/demodeploy/HelloController.java
+++ src/main/java/com/example/demodeploy/HelloController.java
@@ -1,16 +1,21 @@
 package com.example.demodeploy;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping()
 public class HelloController {
+
+    @Value("SOME_ENV_VARIABLE")
+    private String envVariable;
 
     @GetMapping(value = "hello")
     public String hello()  {
@@ -24,5 +29,10 @@ public class HelloController {
         String lbAddress = InetAddress.getLoopbackAddress().getHostAddress();
         String lbHost = InetAddress.getLoopbackAddress().getHostName();
         return "LH: A: " + lhAddress + " H: " + lhHost + " ___ LB: A: " + lbAddress + " H: " + lbHost + ".";
+    }
+
+    @GetMapping(value = "env")
+    public String env() {
+        return "Env variable: " + Optional.ofNullable(envVariable).orElse("undefined");
     }
 }
